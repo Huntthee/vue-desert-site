1c7a13abc800022e7c37083f39fdf6191ff93ee7
Huntthee <mikeallison000@gmail.com>
2022-03-14 17:55:08 -0600

    cleaned out starter code, added placeholder texts

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>About Me</h2>
+  <img src="" alt="A wonderful little image of TheDesertWhale">
+  <p>A few things about me here</p>
+
+  <div>
+    <h4>Skills</h4>
+    <ul>
+      <li>Some Things</li>
+      <li>Other Things</li>
+      <li>More Things</li>
+    </ul>
+  </div>
+</template>
